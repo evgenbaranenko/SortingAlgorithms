9ae88cb57e1e987d4b5dcbb5873d751df5b0721b
evgenbaranenko <evgenbaranenko@gmail.com>
2023-04-19 16:34:31 +0300

    add QuickSort

--- MergeSort/Program.cs
+++ MergeSort/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace MergeSort 
+﻿using System.Collections.Concurrent;
+
+namespace MergeSort
 {
     internal class Program
     {
@@ -11,7 +13,7 @@
             Console.WriteLine();
             ArrayPrint(myArray);
         }
-        
+
         private static void BubleSort_INCRIMENTAL(int[] myArr) // сортировка пузырьком
         {
             var len = myArr.Length;
@@ -43,44 +45,47 @@
                         largestAt = i;
                     }
                 }
-                Swap(array, largestAt,partIndex);
+
+                Swap(array, largestAt, partIndex);
             }
         }
+
         private static void InsertionSort(int[] array) // сортировка вставками
         {
             for (int partIndex = 1; partIndex < array.Length; partIndex++)
             {
                 int curUnsosorted = array[partIndex];
                 int i = 0;
-                for ( i = partIndex; i > 0 && array [i - 1] > curUnsosorted; i--)
+                for (i = partIndex; i > 0 && array[i - 1] > curUnsosorted; i--)
                 {
                     array[i] = array[i - 1];
                 }
+
                 array[i] = curUnsosorted;
             }
         }
 
-        private static void ShellSort(int[] array) // сортировка Шелла (усовершенствованный сорт. вставсками)
+        private static void ShellSort(int[] array) // сортировка Шелла (усовершенствованный сорт. вставками)
         {
             int gap = 1;
-            
-            while (gap < array.Length/3)
-                gap = 3 * gap + 1;  //в gap будет записанно максимальное значение с которого начинаем
-            
+
+            while (gap < array.Length / 3)
+                gap = 3 * gap + 1; //в gap будет записанно максимальное значение с которого начинаем
+
             while (gap >= 1)
             {
                 for (int i = gap; i < array.Length; i++)
                 {
                     for (int j = i; j >= gap && array[j] < array[j - gap]; j -= gap)
                     {
-                        Swap(array,j,j - gap);
+                        Swap(array, j, j - gap);
                     }
                 }
-                
+
                 gap /= 3;
             }
-            
         }
+
         private static void SelectSort(int[] myArr) // сортировка выборкой
         {
             for (int partIndex = myArr.Length - 1; partIndex > 0; partIndex--)
@@ -91,9 +96,11 @@
                     if (myArr[i] > myArr[largestAt])
                         largestAt = i;
                 }
+
                 Swap(myArr, largestAt, partIndex);
             }
         }
+
         public static void MergeSort(int[] array) // сортировка слиянием
         {
             int[] aux = new int[array.Length];
@@ -105,21 +112,21 @@
                     return;
                 int mid = (high + low) / 2;
                 Sort(low, mid);
-                Sort(mid+1,high);
-                Merge(low,mid,high);
+                Sort(mid + 1, high);
+                Merge(low, mid, high);
             }
-            
+
             void Merge(int low, int mid, int high)
             {
-                if(array[mid] <= array [mid + 1]) return;
-                
+                if (array[mid] <= array[mid + 1]) return;
+
                 int i = low;
                 int j = mid + 1;
-                
+
                 Array.Copy(array, low, aux,
-                    low,high - low + 1);
-                
-                for (int k = low; k <=high; k++)
+                    low, high - low + 1);
+
+                for (int k = low; k <= high; k++)
                 {
                     if (i > mid) array[k] = aux[j++];
                     else if (j > high) array[k] = aux[i++];
@@ -129,11 +136,52 @@
                 }
             }
         }
+
+        public static void QuickSort(int[] array) // быстрая сортировка 
+        {
+            Sort(0, array.Length - 1);
+
+            void Sort(int low, int hight)
+            {
+                if (hight <= low) return;
+                int j = Partition(low, hight);
+                Sort(low, j - 1);
+                Sort(j + 1, hight);
+            }
+
+            int Partition(int low, int hight)
+            {
+                int i = low;
+                int j = hight + 1;
+
+                int pivot = array[low];
+
+                while (true)
+                {
+                    while (array[++i] < pivot)
+                        if (i == hight)
+                            break;
+
+                    while (pivot < array[--j])
+                        if (j == low)
+                            break;
+
+                    if (i >= j) break;
+
+                    Swap(array, i, j);
+                }
+
+                Swap(array, low, j);
+
+                return j;
+            }
+        }
         private static void ArrayPrint(int[] myArr)
         {
             for (int i = 0; i < myArr.Length; i++)
                 Console.Write($"{myArr[i]}, ");
         }
+
         private static void Swap(int[] array, int i, int j)
         {
             if (i == j) return;
@@ -143,6 +191,3 @@
         }
     }
 }
-
-
-
